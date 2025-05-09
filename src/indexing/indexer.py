f5c9940feb0ade8414060ac9a8ad86c51380494e
Nino Risteski <ninoristeski@gmail.com>
2025-05-09 21:12:15 +0200

    the indexer with 3 func

--- src/indexing/indexer.py
+++ src/indexing/indexer.py
@@ -9,16 +9,30 @@ class Indexer:
         self.collection_name = collection_name
         self.model_name = model_name
         self.model = SentenceTransformer(model_name)
+        self.client = chromadb.Client()
+        self.collection = self.client.create_collection(collection_name)
 
     def index_repo(self, repo_path: str = "data/tinygrad"):
-        #TODO:
-        pass
+        for root, _, files in os.walk(repo_path):
+            for file in files:
+                if file.endswith(".py"):
+                    with open(os.path.join(root, file), "r") as f:
+                        content = f.read()
+                        embedding = self.model.encode(content).tolist()
+                        self.collection.add(ids=[f"repo_{file}"], embeddings=[embedding], metadatas=[{"source": file, "type": "code"}])
 
     def index_bounties(self, bounty_path: str = "data/bounties.csv"):
-        #TODO:
-        pass
+        bounties = pd.read_csv(bounty_path)
+        for idx, row in bounties.iterrows():
+            content = " ".join(str(v) for v in row.values)
+            embedding = self.model.encode(content).tolist()
+            self.collection.add(ids=[f"bounty_{idx}"], embeddings=[embedding], metadatas=[{"source": "bounties.csv", "type": "bounty"}])
 
     def index_tutorials(self, scraped_path: str = "data/tutorials"):
-        #TODO:
-        pass
+        for file in os.listdir(scraped_path):
+            with open(os.path.join(scraped_path, file), "r") as f:
+                content = f.read()
+                embedding = self.model.encode(content).tolist()
+                self.collection.add(ids=[f"tutorial_{file}"], embeddings=[embedding], metadatas=[{"source": file, "type": "tutorial"}])
+
 
